09d2768ddd694b870a2328d5012c072e13e972a7
xasa <xasa@HURRICANE430S>
2018-10-03 19:14:35 +0800

    文档添加内容2

--- MVCStudy/Controllers/HomeController.cs
+++ MVCStudy/Controllers/HomeController.cs
@@ -3,15 +3,26 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using MVCStudy.Models;
 
 namespace MVCStudy.Controllers
 {
     public class HomeController : Controller
     {
+        Product myProduct = new Product
+        {
+            ProductID = 1,
+            Name = "Bot",
+            Description = "A Robot",
+            Category = "Machine",
+            Price = 19.99M
+        };
+
         // GET: Home
         public ActionResult Index()
         {
-            return View();
+            ViewBag.Ge = "dsdfs";
+            return View(myProduct);
         }
     }
 }
